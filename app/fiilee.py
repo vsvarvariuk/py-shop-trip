044072b9aa9120dd1be82e6d18a533c458b82660
vavarvariuk <vasya.varvariuk.27@gmail.com>
2024-10-23 14:02:07 +0300

    solution

--- app/fiilee.py
+++ app/fiilee.py
@@ -2,12 +2,10 @@ import json
 
 
 class Config:
-    def __init__(self, file_name: str="C:\\Users"
-                                 "\\Користувач"
-                                 "\\enviropment_skill"
-                                 "\\py-shop-trip"
-                                 "\\app"
-                                 "\\config.json") -> None:
+    def __init__(self,
+                 file_name: str = "C:\\Users\\Користувач\\"
+                                  "enviropment_skill\\py-shop-trip\\"
+                                  "app\\config.json") -> None:
         with open(file_name, "r") as f:
             data_file = json.load(f)
         self.fuel_price = data_file["FUEL_PRICE"]
